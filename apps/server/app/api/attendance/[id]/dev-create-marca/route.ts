b4a2bac88974eb2052aa635c59444b883752ba41
AbrJPO98 <abrahamjosue293@gmail.com>
2025-12-23 18:36:56 -0600

    Dev marcas update

--- apps/server/app/api/attendance/[id]/dev-create-marca/route.ts
+++ apps/server/app/api/attendance/[id]/dev-create-marca/route.ts
@@ -10,6 +10,20 @@ export async function POST(req: NextRequest, context: { params: Promise<{ id: st
 
         const { dev } = await req.json();
 
+        let hora_entrada = "06:00:00";
+        let hora_salida = "14:00:00";
+        let tipo_turno = "D";
+        if (toZonedTime(new Date(), "America/Costa_Rica").getHours() >= 14 && toZonedTime(new Date(), "America/Costa_Rica").getHours() < 22) {
+            hora_entrada = "14:00:00";
+            hora_salida = "22:00:00";
+            tipo_turno = "M";
+        }
+        else if (toZonedTime(new Date(), "America/Costa_Rica").getHours() >= 22 && toZonedTime(new Date(), "America/Costa_Rica").getHours() < 6) {
+            hora_entrada = "22:00:00";
+            hora_salida = "06:00:00";
+            tipo_turno = "N";
+        }
+
         const newMarca = await prisma.c_marca_dia.create({
             data: {
                 fecha: toZonedTime(new Date(), "America/Costa_Rica"),
@@ -21,16 +35,16 @@ export async function POST(req: NextRequest, context: { params: Promise<{ id: st
                 horario_id: 1808,
                 plaza_id: 6801,
                 puesto_id: 1728,
-                hora_fin: new Date("1970-01-01 23:00:00"),
-                hora_fin_plan: new Date("1970-01-01 23:00:00"),
-                hora_inicio: new Date("1970-01-01 09:00:00"),
-                hora_inicio_plan: new Date("1970-01-01 09:00:00"),
+                hora_fin: new Date("1970-01-01 " + hora_salida),
+                hora_fin_plan: new Date("1970-01-01 " + hora_salida),
+                hora_inicio: new Date("1970-01-01 " + hora_entrada),
+                hora_inicio_plan: new Date("1970-01-01 " + hora_entrada),
                 horas_duracion: 14,
                 is_dia_excepcion: false,
                 is_puesto_no_cubierto: false,
                 is_reposicion_de_horas: false,
                 teorico: false,
-                tipo_turno: "D",
+                tipo_turno: tipo_turno,
             }
         });
 
